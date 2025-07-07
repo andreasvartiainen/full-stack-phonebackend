f130e74a55f194b621eaf8e878a2d5dd62b15f1b
Tomi <59865056+andreasvartiainen@users.noreply.github.com>
2025-07-07 16:48:54 +0300

    3.3 done

--- index.js
+++ index.js
@@ -43,6 +43,16 @@ app.get('/api/persons', (request, response) => {
 	response.json(data);
 })
 
+app.get('/api/persons/:id', (request, response) => {
+	const id = request.params.id;
+	const person = data.find((d) => d.id === id)
+	if (person)
+		response.json(person);
+	else
+		response.statusMessage = `Person with id ${id} not found`
+		response.status(404).send();
+})
+
 const PORT = 3001;
 app.listen(PORT,() => {
 	console.log(`Server running on port ${PORT}`);
