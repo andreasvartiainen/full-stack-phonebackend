9587b496ce8742fccf599a27b9a59661c07d6002
Tomi <59865056+andreasvartiainen@users.noreply.github.com>
2025-07-07 16:44:37 +0300

    3.2 exercise done

---
+++ index.js
@@ -0,0 +1,49 @@
+const express = require('express');
+const app = express();
+app.use(express.json());
+
+const data = [
+    { 
+      "id": "1",
+      "name": "Arto Hellas", 
+      "number": "040-123456"
+    },
+    { 
+      "id": "2",
+      "name": "Ada Lovelace", 
+      "number": "39-44-5323523"
+    },
+    { 
+      "id": "3",
+      "name": "Dan Abramov", 
+      "number": "12-43-234345"
+    },
+    { 
+      "id": "4",
+      "name": "Mary Poppendieck", 
+      "number": "39-23-6423122"
+    }
+]
+
+app.get('/', (request, response) => {
+	response.send('<h1>Hello</h1>');
+})
+
+app.get('/info', (request, response) => {
+	const date = new Date(Date.now()).toString()
+	const page = `
+		<div>Phonebook has info for ${data.length} people</div>
+		<div>${date}</div>
+	`
+	response.send(page);
+})
+
+app.get('/api/persons', (request, response) => {
+	console.log(request);
+	response.json(data);
+})
+
+const PORT = 3001;
+app.listen(PORT,() => {
+	console.log(`Server running on port ${PORT}`);
+})
